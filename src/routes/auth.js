15cb32c1129fedb4fcfef33b8d219306dcfd7629
BrettS89 <blsodie@gmail.com>
2019-04-05 20:13:37 -0400

    Added facebook auth endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,8 @@ router.post('/signup', auth.signup);
 
 router.post('/login', auth.login);
 
+router.post('/facebook', auth.facebookAuth);
+
 router.get('/isloggedin', auth.isLoggedIn);
 
 module.exports = router;

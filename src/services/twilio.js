4a295d4732adc71856a4a8db43eaae52b1f192b0
BrettS89 <blsodie@gmail.com>
2019-10-04 18:16:09 -0400

    filtering our ids

--- src/services/twilio.js
+++ src/services/twilio.js
@@ -1,5 +1,6 @@
 const twilio = require('twilio');
 const keys = require('../config');
+const ourIds = ['5ca384ccf28c230017f763e6', '5cb2941be3a7ac00177714e3', '5cc231b33da1a6001797d338'];
 
 const client = new twilio(keys.twilioAccountSid, keys.twilioAuthToken);
 
@@ -15,12 +16,14 @@ exports.signupSMS = name => {
   }
 };
 
-exports.loginSMS = name => {
-  const body = `${name} just opened the app`;
+exports.loginSMS = user => {
+  const body = `${user.firstName} just opened the app`;
   if (keys.environment === 'production') {
     try {
-      client.messages.create({ body, to: '+16092131708', from: '+12679145215' });
-      client.messages.create({ body, to: '+16098024463', from: '+12679145215' });
+      if (!ourIds.includes(user._id.toString())) {
+        client.messages.create({ body, to: '+16092131708', from: '+12679145215' });
+        client.messages.create({ body, to: '+16098024463', from: '+12679145215' });
+      }
     } catch(e) {
       console.log(e);
     }

ef82762d8b4ffd3a4aa0cf413eef579981b985f7
BrettS89 <blsodie@gmail.com>
2019-10-04 17:36:17 -0400

    fixing trending route

--- src/routes/trending.js
+++ src/routes/trending.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const trendingControllers = require('../controllers/trending');
 
-router.get('trending', trendingControllers.getTop25);
+router.get('/get', trendingControllers.getTop25);
 
 module.exports = router;
